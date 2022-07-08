28674124f49ef916db2d5879eca47e459f315837
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-07-08 11:57:27 -0400

    handle controlling backdrop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,26 @@
 const addMovieModal = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector("header button")
 
+const backdrop = document.getElementById("backdrop")
+
+const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
+const toggleBackdrop = () => {
+    backdrop.classList.toggle("visible");
+}
+
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle("visible");
+    toggleBackdrop();
 }
-startAddMovieButton.addEventListener('click', )
+
+const backdropClickHandler = () => {
+    toggleMovieModal();
+}
+
+const cancelAddMovie = () => {
+    toggleMovieModal()
+}
+
+startAddMovieButton.addEventListener('click', toggleMovieModal)
+backdrop.addEventListener('click', backdropClickHandler)
+cancelAddMovieButton.addEventListener('click', cancelAddMovie )
